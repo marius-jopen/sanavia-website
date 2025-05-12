09b102522281aea3f259d7160b37553791fc33bb
Marius Jopen <mail@mariusjopen.com>
2025-05-12 14:31:07 +0200

    LINT

--- src/slices/Video/index.tsx
+++ src/slices/Video/index.tsx
@@ -4,7 +4,6 @@ import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { setupFadeInAnimation } from "../../utils/animations/intersectionAnimations";
 import VideoBasic from "../../components/VideoBasic";
-import { PrismicNextImage } from "@prismicio/next";
 
 /**
  * Props for `Video`.
