a527372f770f0cbb96ce7b72ae88bc38ad5feaa4
Marius Jopen <mail@mariusjopen.com>
2025-06-02 16:34:50 +0200

    space

--- src/slices/Space/index.tsx
+++ src/slices/Space/index.tsx
@@ -11,12 +11,12 @@ export type SpaceProps = SliceComponentProps<Content.SpaceSlice>;
  * Height mapping for different sizes using Tailwind responsive classes
  */
 const heightMap = {
-  "xs": "h-4 md:h-8",
-  "s": "h-8 md:h-16", 
-  "m": "h-8 md:h-24",
-  "lg": "h-12 md:h-32",
-  "xl": "h-12 md:h-52",
-  "2xl": "h-16 md:h-64",
+  "xs": "h-2 md:h-8",
+  "s": "h-2 md:h-16", 
+  "m": "h-2 md:h-24",
+  "lg": "h-4 md:h-32",
+  "xl": "h-8 md:h-52",
+  "2xl": "h-40 md:h-64",
 };
 
 /**
