46c897b9944882dded119efb1046441968c0b83e
Marius Jopen <mail@mariusjopen.com>
2025-08-19 10:32:38 +0200

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Background from "@/components/Background";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import Script from "next/script";
 
 export default async function RootLayout({
   children,
@@ -16,6 +17,18 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className="w-full overflow-x-hidden">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9WB5C8BYNX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9WB5C8BYNX');
+          `}
+        </Script>
         <Header data={settings.data} />
         {children}
         <Footer data={settings.data} />
